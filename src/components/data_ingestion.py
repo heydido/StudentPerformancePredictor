7f63d793f097d2206ece38ab3f7cbba45f2ce4f6
heydido <aashish4.iitd@gmail.com>
2023-03-20 00:10:33 +0530

    Model trainer is completed.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 from src.exception import CustomException
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 
 @dataclass
@@ -39,7 +40,7 @@ class DataIngestion:
             return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
 
         except Exception as e:
-           raise CustomException(e, sys)
+            raise CustomException(e, sys)
 
 
 if __name__ == '__main__':
@@ -47,5 +48,8 @@ if __name__ == '__main__':
     train_path, test_path = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_path, test_path)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
 
+    model_trainer = ModelTrainer()
+    r2_square_best_model = model_trainer.initiate_model_training(train_arr, test_arr)
+    print(r2_square_best_model)
