eb40d1146335a100e23efedc069ccb73b9e0551f
heydido <aashish4.iitd@gmail.com>
2023-03-19 20:01:05 +0530

    Data Transformation pipeline completed.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.logger import logging
 from dataclasses import dataclass
 from src.exception import CustomException
 from sklearn.model_selection import train_test_split
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 
 @dataclass
@@ -38,9 +39,13 @@ class DataIngestion:
             return self.ingestion_config.train_data_path, self.ingestion_config.test_data_path
 
         except Exception as e:
-            raise CustomException(e, sys)
+           raise CustomException(e, sys)
 
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_path, test_path)
+
