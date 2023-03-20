7be2e0b34f0999b0d4509ed2d2ee5f0b3959cbbb
heydido <aashish4.iitd@gmail.com>
2023-03-20 16:52:24 +0530

    Completed prediction_pipeline

--- src/utils.py
+++ src/utils.py
@@ -45,3 +45,12 @@ def evaluate_models(x_train, y_train, x_test, y_test, models, param):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_object:
+            return dill.load(file_object)
+
+    except Exception as e:
+        raise CustomException(e, sys)
