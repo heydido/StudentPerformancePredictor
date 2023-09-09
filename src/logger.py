ca90808a4cf0e2fe5021d1d956f2ac3226f85817
heydido <aashish4.iitd@gmail.com>
2023-09-09 17:23:44 +0530

    Updated logger.py to create folder name without .log, and corrected importing logger in exception.py

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE.split('.')[0])
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
@@ -18,4 +18,3 @@ logging.basicConfig(
 
 if __name__ == '__main__':
     logging.info('Logging has started!')
-
