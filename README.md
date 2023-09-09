1e7223cff5055f51e334a4d51e6218b848a35504
heydido <aashish4.iitd@gmail.com>
2023-09-09 16:36:24 +0530

    updated req.txt, README and setup.py

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## Student Performance Predictor
+# Student Performance Predictor
+
+## Setting up virtual environment
+    conda create -p venv python==3.8
+    conda activate venv/
+
--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ def get_requirements(file_path: str) -> List[str]:
     """
     This function will return the list of requirements.
     """
-    requirements = []
+
     with open(file_path) as f:
         requirements = f.readlines()
-        requirements = [req.replace('\n', " ") for req in requirements]
+        requirements = [req.replace('\n', "") for req in requirements]
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
@@ -19,7 +19,7 @@ def get_requirements(file_path: str) -> List[str]:
 
 
 setup(
-    name='end-to-end-mlproject',
+    name='student-performance-predictor',
     version='0.0.1',
     author='heydido',
     author_email='aashish4.iitd@gmail.com',
