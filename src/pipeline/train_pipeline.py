71cdeea4f5deaa6b0a4e3e918fb78b3fcd5ea66e
heydido <aashish4.iitd@gmail.com>
2023-09-17 12:50:37 +0530

    Updated train_pipeline

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,14 +1,33 @@
+import sys
+from src.logger import logging
+from src.exception import CustomException
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
+
+class TrainPipeline:
+    def __init__(self) -> None:
+        self.data_ingestor = DataIngestion()
+        self.data_transformer = DataTransformation()
+        self.model_trainer = ModelTrainer()
+
+    def run_pipeline(self) -> float:
+        try:
+            logging.info('Initiating training pipeline ...')
+            train_path, test_path = self.data_ingestor.initiate_data_ingestion()
+            train_array, test_array, _ = self.data_transformer.initiate_data_transformation(
+                train_path=train_path, test_path=test_path
+            )
+            best_r2_score = self.model_trainer.initiate_model_training(train_array=train_array, test_array=test_array)
+
+            return best_r2_score
+
+        except Exception as e:
+            logging.info('Exception occurred at training pipeline stage.')
+            raise CustomException(e, sys)
+
+
 if __name__ == '__main__':
-    data_ingestor = DataIngestion()
-    train_data_path, test_data_path = data_ingestor.initiate_data_ingestion()
-    print('train_data_path: ', train_data_path, 'test_data_path: ', test_data_path)
-    data_transformer = DataTransformation()
-    train_arr, test_arr, preprocessor_obj_path = data_transformer.initiate_data_transformation(
-        train_path=train_data_path, test_path=test_data_path
-    )
-    model_trainer = ModelTrainer()
-    best_r2_score = model_trainer.initiate_model_training(train_array=train_arr, test_array=test_arr)
+    train_pipeline = TrainPipeline()
+    train_pipeline.run_pipeline()
